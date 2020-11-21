a4bb59a4b9bab4562d0878919d8fba1ae9951a72
Edward <eddy.crowder@gmail.com>
2020-11-20 19:17:32 -0500

    Feat GetUsername added
    Created method to get username of currently signed in user and added Doc strings to other security methods

--- security_features.py
+++ security_features.py
@@ -1,16 +1,17 @@
-import os
-
+import os, pwd
 class EnterpriseShield:
 
     def __init__(self):
         self.notification_timer = 0
 
     def notify(self, title, text):
+        ''' executes apple script to generate system timer'''
         os.system("""
                   osascript -e 'display notification "{}" with title "{}"'
                   """.format(text, title))
 
     def notify_reset_timer(self, name):
+        ''' Resets timer to show system notification '''
         if self.notification_timer <= 0 and name.lower() == "unknown":
             self.notification_timer = 60
             self.notify("Shoulder Surfing Detected", "Quick behind you!")
@@ -18,11 +19,17 @@ class EnterpriseShield:
             self.notification_timer -= 1
 
     def hide_windows(self):
+        ''' executes apple script to hide all active windows. '''
         os.system("""
         osascript -e 'tell application "System Events" to set visible of every application process to false'
                 """)
         
     def show_windows(self):
+        ''' executes apple script to show all active windows. '''
         os.system("""
         osascript -e 'tell application "System Events" to set visible of every application process to true'
                 """)
+
+    def get_username(self):
+        ''' Returns the currently signed in username as <string> '''
+        return pwd.getpwuid( os.getuid() )[ 0 ]
