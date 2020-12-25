46c877e73b916726c690ce1ef45e54cc52c59ad3
Ahmad Chaiban <ahmadchaiban@gmail.com>
2020-12-25 18:34:32 +0300

    Feat (windows security scripts)
    1. Three powershell scripts that have the same functions as the apple scripts.
    2. Create a python file that holds them

--- security_features.py
+++ security_features.py
@@ -20,7 +20,6 @@ class EnterpriseShield:
                   osascript -e 'display notification "{}" with title "{}"'
                   """.format(text, title))
 
-
     def notify_reset_timer(self):
         ''' Resets timer to show system notification '''
         if self.notification_timer <= 0:
@@ -29,13 +28,11 @@ class EnterpriseShield:
         else:
             self.notification_timer -= 1
 
-
     def hide_windows(self):
         ''' executes apple script to hide all active windows. '''
         os.system("""
         osascript -e 'tell application "System Events" to set visible of every application process to false'
                 """)
-
 
     def show_windows(self):
         ''' executes apple script to show all active windows. '''
@@ -43,11 +40,9 @@ class EnterpriseShield:
         osascript -e 'tell application "System Events" to set visible of every application process to true'
                 """)
 
-
     def get_username(self):
         ''' Returns the currently signed in username as <string> '''
         return pwd.getpwuid( os.getuid() )[ 0 ]
-    
 
     def lock_screen(self):
         ''' executes apple script to lock the screen and require user to login again '''
